8153dd3022e1adb457bef33d9b2701d3693cc5f7
ProudMay <mehdi79nekouei@gmail.com>
2025-11-12 16:12:56 +0330

    feat [ add header and 3 btn ]

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
 import "./style.css";
+
+alert("salam ziba !");
+
+const header = document.createElement('header');
+header.innerHTML = `
+<nav>
+<button id="home"> Home </button>
+<button id="menu"> Menu </button>
+<button id="about"> About us </button>
+</nav>
+
+`
+document.body.prepend(header);
